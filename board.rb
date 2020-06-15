a73fee7047fd40cb0c560e48c4cdd94874375f66
Andrea Silva <andrea@DESKTOP-75HTEFS.localdomain>
2020-06-15 16:15:12 -0500

    Updated my if's to find the neighbours of the cell

--- board.rb
+++ board.rb
@@ -11,35 +11,56 @@ class Board
   end
 
   def read
+    temp_board = @board.dup
     (0..9).each do |row|
       (0..9).each do |column|
         cell = @board[row, column]
         status = Cell.new.alive?(cell)
+        alive_neighbours = neighbours(row,column)
+        if status && alive_neighbours >=4
+          temp_board[row, column] = 0
+        elsif status && alive_neighbours >=2
+          temp_board[row, column] = 1
+        elsif status && alive_neighbours <=1
+          temp_board[row, column] = 0
+        elsif status == false && alive_neighbours == 3
+          temp_board[row, column] = 1
+        end
       end
     end
+    @board = temp_board
   end
 
   def neighbours(row, column)
     neighbours = []
     neighbours.push(@board[row-1, column]) if inside_board?(row-1, column)
     neighbours.push(@board[row-1, column+1]) if inside_board?(row-1, column+1)
-    neighbours.push(@board[row-1, column+1]) if inside_board?(row-1, column+1)
-    puts neighbours
+    neighbours.push(@board[row, column+1]) if inside_board?(row, column+1)
+    neighbours.push(@board[row+1, column+1]) if inside_board?(row+1, column+1)
+    neighbours.push(@board[row+1, column]) if inside_board?(row+1, column)
+    neighbours.push(@board[row+1, column-1]) if inside_board?(row+1, column-1)
+    neighbours.push(@board[row, column-1]) if inside_board?(row, column-1)
+    neighbours.push(@board[row-1, column-1]) if inside_board?(row-1, column-1)
+    neighbours.count(1)
   end
 
   def inside_board?(row, column)
     row < @size && row >= 0 && column < @size && column >= 0
   end
-
+ 
   def print_board
-    print @board
+    # Esto nos ayuda para hacer la matrix en cuadrito
+    @board.to_a.each {|r| puts r.inspect}
   end
 end
 
-
+class Matrix
+  public :"[]=", :set_element, :set_component
+end
 board = Board.new
 
 board.build
-board.read
 board.print_board
-board.neighbours(1, 0)
+board.read
+puts "= "*50
+board.print_board
